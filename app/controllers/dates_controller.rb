63dc33e1451e07a496a0e2c1e989eff5cbdb696b
Arnaud Lenglet <arlenglet@gmail.com>
2020-05-01 18:11:32 +0200

    fix(controller): remove placeholder date from DatesController#Parse

--- app/controllers/dates_controller.rb
+++ app/controllers/dates_controller.rb
@@ -1,7 +1,8 @@
 class DatesController < ApplicationController
   def parse
     parsed = Chronic.parse(params['date'], hours24: true)
-    date_only = Date.parse("2020-04-28T12:00:00.000+02:00").to_s
+    date_only = Date.parse(parsed).to_s
+
     render json: {
       source: params['date'],
       date: date_only,
