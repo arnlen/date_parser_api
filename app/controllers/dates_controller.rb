6a7ca9301f454a74331725c757142883c7df5c10
Arnaud Lenglet <arlenglet@gmail.com>
2020-05-01 16:53:08 +0200

    feat(api): returns date & datetime formats from DatesController

--- app/controllers/dates_controller.rb
+++ app/controllers/dates_controller.rb
@@ -1,6 +1,11 @@
 class DatesController < ApplicationController
   def parse
     parsed = Chronic.parse(params['date'], hours24: true)
-    render json: parsed
+    date_only = Date.parse("2020-04-28T12:00:00.000+02:00").to_s
+    render json: {
+      source: params['date'],
+      date: date_only,
+      datetime: parsed,
+    }
   end
 end
