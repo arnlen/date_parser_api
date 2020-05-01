2c17d08f2e918f6fa6bd17062d73f066cc02fec2
Arnaud Lenglet <arlenglet@gmail.com>
2020-05-01 18:25:52 +0200

    fix(controller): fix date conversion in DatesController#Parse

--- app/controllers/dates_controller.rb
+++ app/controllers/dates_controller.rb
@@ -1,12 +1,21 @@
 class DatesController < ApplicationController
   def parse
-    parsed = Chronic.parse(params['date'], hours24: true)
-    date_only = Date.parse(parsed).to_s
+    parsed = Chronic.parse(date_params['date'], hours24: true)
+    Rails.logger.info("ℹ️ Params: #{date_params}")
+    Rails.logger.info("ℹ️ Parsed: #{parsed}")
+
+    date_only = parsed.to_date.to_s
+
+    Rails.logger.info("ℹ️ Date only: #{date_only}")
 
     render json: {
-      source: params['date'],
+      source: date_params['date'],
       date: date_only,
       datetime: parsed,
     }
+  end
+
+  def date_params
+    params.permit(:date)
   end
 end
