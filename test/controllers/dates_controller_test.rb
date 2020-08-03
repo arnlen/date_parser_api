23858bd96a8eb51421b2813fbb003ca0d2f362c1
Arnaud Lenglet <arlenglet@gmail.com>
2020-08-03 17:49:57 +0200

    feat(dates/routes): update route file to allow multiple api endpoints

--- test/controllers/dates_controller_test.rb
+++ test/controllers/dates_controller_test.rb
@@ -8,84 +8,84 @@ class DatesControllerTest < ActionDispatch::IntegrationTest
     date: '2020-08-05'
   }
 
-  test "Root path without params" do
-    get root_url
+  test "Without params" do
+    get dates_parse_url
     json_response = JSON.parse(response.body)
 
     assert_response :success
     assert_equal '', json_response['date']
   end
 
-  test "Root path with empty date" do
+  test "Empty date" do
     source_date = ''
 
-    get root_url, params: { date: source_date }
+    get dates_parse_url, params: { date: source_date }
     json_response = JSON.parse(response.body)
 
     assert_response :success
     assert_equal '', json_response['date']
   end
 
-  test "Root path with 8/05/20" do
+  test "8/05/20" do
     source_date = '8/05/20'
     expected_response[:date] = '2020-08-05'
 
-    get root_url, params: { date: source_date }
+    get dates_parse_url, params: { date: source_date }
     json_response = JSON.parse(response.body)
 
     assert_response :success
     assert_equal expected_response[:date], json_response['date']
   end
 
-  test "Root path with 08/05/2020" do
+  test "08/05/2020" do
     source_date = '08/05/2020'
     expected_response[:date] = '2020-08-05'
 
-    get root_url, params: { date: source_date }
+    get dates_parse_url, params: { date: source_date }
     json_response = JSON.parse(response.body)
 
     assert_response :success
     assert_equal expected_response[:date], json_response['date']
   end
 
-  test "Root path with 2020-05-08" do
+  test "2020-05-08" do
     source_date = '2020-05-08'
     expected_response[:date] = '2020-05-08'
 
-    get root_url, params: { date: source_date }
+    get dates_parse_url, params: { date: source_date }
     json_response = JSON.parse(response.body)
 
     assert_response :success
     assert_equal expected_response[:date], json_response['date']
   end
 
-  test "Root path with 5 mai 2020" do
+  test "5 mai 2020" do
     source_date = '5 mai 2020'
     expected_response[:date] = '2020-05-05'
 
-    get root_url, params: { date: source_date }
+    get dates_parse_url, params: { date: source_date }
     json_response = JSON.parse(response.body)
 
     assert_response :success
     assert_equal expected_response[:date], json_response['date']
   end
 
-  test "Root path with 5 avril 2020" do
+  test "5 avril 2020" do
     source_date = '5 avril 2020'
     expected_response[:date] = '2020-04-05'
 
-    get root_url, params: { date: source_date }
+    get dates_parse_url, params: { date: source_date }
     json_response = JSON.parse(response.body)
 
     assert_response :success
     assert_equal expected_response[:date], json_response['date']
   end
 
-  test "Root path with 5 avr. 2020" do
+  test "5 avr. 2020" do
     source_date = '5 avr. 2020'
     expected_response[:date] = '2020-04-05'
 
-    get root_url, params: { date: source_date }
+    get dates_parse_url, params: { date: source_date }
     json_response = JSON.parse(response.body)
 
     assert_response :success
