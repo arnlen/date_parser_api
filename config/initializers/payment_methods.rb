6d2f4c5f4324fd75776366b2292f74aa7d5449f8
Arnaud Lenglet <arlenglet@gmail.com>
2020-08-03 17:49:20 +0200

    config(PaymentMethods): add PaymentMethods constants

---
+++ config/initializers/payment_methods.rb
@@ -0,0 +1,28 @@
+module PaymentMethods
+
+  CB_METHOD = [
+    'cb',
+    'carte',
+    'carte bancaire',
+    'carte bleue',
+    'card',
+    'visa',
+    'credit card',
+    'carte de crédit'
+  ]
+
+  TRANSFER_METHOD = [
+    'virement',
+    'virement bancaire',
+    'rib'
+  ]
+
+  DEBIT_METHOD = [
+    'prélèvement',
+    'prélèvement automatique',
+    'prélevé',
+    'débité',
+    'débité automatiquement'
+  ]
+
+end
