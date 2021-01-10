41215c11c1bb8a8e74d2b0cd1ef4bf41c8576cf8
Arnaud Lenglet <arnaud@pandaguidesme.com>
2021-01-10 23:49:34 +0100

    feat(crypto): create CryptosController + get quotes from CMC

---
+++ app/controllers/cryptos_controller.rb
@@ -0,0 +1,41 @@
+require 'net/http'
+
+class CryptosController < ApplicationController
+
+  def get_latest_quotes_for
+    tokens = crypto_params['tokens']
+
+    render json: get_quotes_from_coinmarketcap_for(tokens)
+  end
+
+  private
+
+  def get_quotes_from_coinmarketcap_for(tokens)
+    base_url = Rails.env.development? ? 'sandbox-api' : 'pro-api'
+    api_key = Rails.env.development? ? 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c' : ENV['COIN_MARKET_CAP_API_KEY']
+    url_params = "symbol=#{tokens}&convert=eur"
+    uri = URI("https://#{base_url}.coinmarketcap.com/v1/cryptocurrency/quotes/latest?#{url_params}")
+
+    request = Net::HTTP::Get.new(uri)
+    request['X-CMC_PRO_API_KEY'] = api_key # CMC auth headers
+
+    response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    body = JSON.parse(response.body)
+
+    quotes = {}
+
+    body['data'].each do |token_data|
+      quotes["#{token_data[1]['symbol']}"] = token_data[1]['quote']['EUR']['price']
+    end
+
+    quotes
+  end
+
+  def crypto_params
+    params.permit(:tokens)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'amounts/parse'
   get 'dates/parse'
   get 'payment_methods/parse'
+  get 'cryptos/get_latest_quotes_for'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
